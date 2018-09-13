bc56c7c42b2d75e0a6cfc27b3c82af5d260345c1
xuexiang <15308367519@163.com>
2018-09-13 18:30:04 +0800

    project
    first

---
+++ server/src/main/java/com/ServerApplication.java
@@ -0,0 +1,16 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@EnableEurekaClient
+@SpringBootApplication
+@EnableDiscoveryClient
+public class ServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServerApplication.class, args);
+    }
+}
