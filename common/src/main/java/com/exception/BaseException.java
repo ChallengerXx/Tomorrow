67cfddf4713c6c909556af05261b3f00b09d709d
xuexiang <15308367519@163.com>
2018-09-17 10:56:09 +0800

    公共异常类

---
+++ common/src/main/java/com/exception/BaseException.java
@@ -0,0 +1,22 @@
+package com.exception;
+
+import com.EnumBean.CommonEnum;
+
+/**
+ * 公共异常类
+ */
+public class BaseException extends RuntimeException {
+
+    private String msg;
+    private String code;
+
+    public static BaseException fail() {
+        return new BaseException(CommonEnum.FAIL.getCode(), CommonEnum.FAIL.getMsg());
+    }
+
+    public BaseException(String msg, String code) {
+        super(msg);
+        this.msg = msg;
+        this.code = code;
+    }
+}
