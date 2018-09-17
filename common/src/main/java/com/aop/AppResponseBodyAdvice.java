7b62ba40ba803119761dc23f034acf6b81a68a0a
xuexiang <15308367519@163.com>
2018-09-17 10:53:43 +0800

    添加返回结果处理advice

---
+++ common/src/main/java/com/aop/AppResponseBodyAdvice.java
@@ -0,0 +1,54 @@
+package com.aop;
+
+import com.EnumBean.CommonEnum;
+import com.bean.BaseResp;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.MethodParameter;
+import org.springframework.http.MediaType;
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
+
+@RestControllerAdvice
+@Slf4j
+public class AppResponseBodyAdvice implements ResponseBodyAdvice {
+
+    /**
+     * 判断支持类型
+     *
+     * @param methodParameter
+     * @param aClass
+     * @return
+     */
+    @Override
+    public boolean supports(MethodParameter methodParameter, Class aClass) {
+//        if (!methodParameter.getMethod().getReturnType().isAssignableFrom(AppResponseBodyAdvice.class)) {
+//            log.error("支持类型错误 -->" + methodParameter.getMethod().getReturnType().getClass());
+//            return false;
+//        }
+        return true;
+    }
+
+    /**
+     * 对结果进行包装
+     *
+     * @param obj
+     * @param methodParameter
+     * @param mediaType
+     * @param aClass
+     * @param req
+     * @param resp
+     * @return
+     */
+    @Override
+    public Object beforeBodyWrite(Object obj, MethodParameter methodParameter, MediaType mediaType, Class aClass,
+                                  ServerHttpRequest req, ServerHttpResponse resp) {
+        resp.getHeaders().add("Content-type", "application/json");
+        if (obj != null && obj.getClass().isAssignableFrom(BaseResp.class)) {
+            return obj;
+        }
+        BaseResp resp1 = new BaseResp(CommonEnum.SUCCESS.getCode(),CommonEnum.SUCCESS.getMsg(),obj);
+        return resp1;
+    }
+}
